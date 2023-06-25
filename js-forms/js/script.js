7e1735d04569116b0a364feeb3a88111a7f80787
Arto <example@gmail.com>
2023-06-25 15:38:17 +0400

    first commit

---
+++ js-forms/js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Hello ${user}`);
+
+// console.log('arr' + ' - object');
+// console.log(4 + '5'); 
+
+// let incr = 10;
+// let decr = 10;
+
+// // ++incr;
+// // --decr;
+
+
+// console.log(++incr);
+// console.log(--decr);
+
+
+// console.log(5%2);
+
+// console.log(2 + 2 * 2 !== "6");
+
+
+// const isCheck = false;
+// const isClose = false;
+
+// console.log(isCheck || !isClose);
+
+
+
+
